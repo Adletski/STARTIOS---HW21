dff96d967da2a552cdb12123e46e5aa071968c40
Adlet <adlet.zh19966@gmail.com>
2023-05-24 10:10:43 +0600

    Initial Commit

---
+++ tb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tb
+//
+//  Created by Adlet Zhantassov on 24.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
